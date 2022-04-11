a0077258b79659f9d18853b5aa4f7bf32311df16
marco.brasci@telenetgroup.be <marco.brasci@telenetgroup.be>
2022-04-11 22:31:10 +0200

    Creating a service for each ProductType using Strategy Design Pattern

--- Java/src/main/java/com/gildedrose/productservice/NormalProductService.java
+++ Java/src/main/java/com/gildedrose/productservice/NormalProductService.java
@@ -4,6 +4,9 @@ import com.gildedrose.Item;
 import com.gildedrose.ProductService;
 import com.gildedrose.ProductType;
 
+/**
+ * TODO Check Aged Brie + Elixer of Goose (Normal product)
+ */
 public class NormalProductService implements ProductService {
 
     /**
