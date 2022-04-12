0d547b9dc04ca56b6cb2f7f0b51ae444952a6e53
marco.brasci@telenetgroup.be <marco.brasci@telenetgroup.be>
2022-04-12 20:47:35 +0200

    Added tests

--- Java/src/test/java/com/gildedrose/productservice/BackstageProductServiceTest.java
+++ Java/src/test/java/com/gildedrose/productservice/BackstageProductServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class BackstageProductServiceTest {
+class BackstageProductServiceTest extends AbstractProductServiceTest {
 
     private BackstageProductService backstageProductService;
 
@@ -20,107 +20,69 @@ class BackstageProductServiceTest {
     void dayAfterConcert() {
         Item item = new Item(ProductConstants.PRODUCT_BACKSTAGE_PASSES_TO_CONCERT, 0, 50);
 
-        backstageProductService.updateItem(item);
-
-        assertThat(item.sellIn).isEqualTo(-1);
-        assertThat(item.quality).isEqualTo(0);
+        advanceDay(backstageProductService, item, -1, 0);
     }
 
     @Test
     void concertIsLessThen5Days() {
         Item item = new Item(ProductConstants.PRODUCT_BACKSTAGE_PASSES_TO_CONCERT, 4, 20);
 
-        backstageProductService.updateItem(item);
-
-        assertThat(item.sellIn).isEqualTo(3);
-        assertThat(item.quality).isEqualTo(23);
+        advanceDay(backstageProductService, item, 3, 23);
+        advanceDay(backstageProductService, item, 2, 26);
+        advanceDay(backstageProductService, item, 1, 29);
     }
 
     @Test
     void concertIsLessThen10Days() {
         Item item = new Item(ProductConstants.PRODUCT_BACKSTAGE_PASSES_TO_CONCERT, 10, 20);
 
-        backstageProductService.updateItem(item);
+        advanceDay(backstageProductService, item, 9, 22);
+        advanceDay(backstageProductService, item, 8, 24);
+        advanceDay(backstageProductService, item, 7, 26);
 
-        assertThat(item.sellIn).isEqualTo(9);
-        assertThat(item.quality).isEqualTo(22);
     }
 
     @Test
-    void concertIsLessMore10DaysAway() {
+    void concertIsMore10DaysAway() {
         Item item = new Item(ProductConstants.PRODUCT_BACKSTAGE_PASSES_TO_CONCERT, 15, 20);
 
-        backstageProductService.updateItem(item);
-
-        assertThat(item.sellIn).isEqualTo(14);
-        assertThat(item.quality).isEqualTo(21);
+        advanceDay(backstageProductService, item, 14, 21);
+        advanceDay(backstageProductService, item, 13, 22);
+        advanceDay(backstageProductService, item, 12, 23);
     }
 
     @Test
     void qualityCannotGoAbove50() {
-        Item item = new Item(ProductConstants.PRODUCT_BACKSTAGE_PASSES_TO_CONCERT, 1, 50);
+        Item item = new Item(ProductConstants.PRODUCT_BACKSTAGE_PASSES_TO_CONCERT, 2, 49);
 
-        backstageProductService.updateItem(item);
-
-        assertThat(item.sellIn).isEqualTo(0);
-        assertThat(item.quality).isEqualTo(50);
+        advanceDay(backstageProductService, item, 1, 50);
+        advanceDay(backstageProductService, item, 0, 50);
     }
 
     @Test
     void fullCountDown() {
         Item item = new Item(ProductConstants.PRODUCT_BACKSTAGE_PASSES_TO_CONCERT, 11, 10);
 
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(10);
-        assertThat(item.quality).isEqualTo(11);
+        // >10 = +1
+        advanceDay(backstageProductService, item, 10, 11);
 
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(9);
-        assertThat(item.quality).isEqualTo(13);
+        // between 5 and 10 = +2
+        advanceDay(backstageProductService, item, 9, 13);
+        advanceDay(backstageProductService, item, 8, 15);
+        advanceDay(backstageProductService, item, 7, 17);
+        advanceDay(backstageProductService, item, 6, 19);
+        advanceDay(backstageProductService, item, 5, 21);
 
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(8);
-        assertThat(item.quality).isEqualTo(15);
+        // between 0 and 5
+        advanceDay(backstageProductService, item, 4, 24);
+        advanceDay(backstageProductService, item, 3, 27);
+        advanceDay(backstageProductService, item, 2, 30);
+        advanceDay(backstageProductService, item, 1, 33);
+        advanceDay(backstageProductService, item, 0, 36);
 
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(7);
-        assertThat(item.quality).isEqualTo(17);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(6);
-        assertThat(item.quality).isEqualTo(19);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(5);
-        assertThat(item.quality).isEqualTo(21);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(4);
-        assertThat(item.quality).isEqualTo(24);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(3);
-        assertThat(item.quality).isEqualTo(27);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(2);
-        assertThat(item.quality).isEqualTo(30);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(1);
-        assertThat(item.quality).isEqualTo(33);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(0);
-        assertThat(item.quality).isEqualTo(36);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(-1);
-        assertThat(item.quality).isEqualTo(0);
-
-        backstageProductService.updateItem(item);
-        assertThat(item.sellIn).isEqualTo(-2);
-        assertThat(item.quality).isEqualTo(0);
+        // days after concert
+        advanceDay(backstageProductService, item, -1, 0);
+        advanceDay(backstageProductService, item, -2, 0);
     }
 
     @Test
--- Java/src/test/java/com/gildedrose/productservice/SulfurasProductServiceTest.java
+++ Java/src/test/java/com/gildedrose/productservice/SulfurasProductServiceTest.java
@@ -1,17 +1,28 @@
 package com.gildedrose.productservice;
 
+import com.gildedrose.Item;
+import com.gildedrose.ProductConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class SulfurasProductServiceTest {
+class SulfurasProductServiceTest extends AbstractProductServiceTest {
 
     private SulfurasProductService sulfurasProductService = new SulfurasProductService();
 
     @BeforeEach
     void setUp() {
         sulfurasProductService = new SulfurasProductService();
+    }
+
+    @Test
+    void sulfurasIsNeverSoldQualityRemains80() {
+        Item item = new Item(ProductConstants.PRODUCT_SULFURAS_HAND_OF_RAGNAROS, 5, 80);
+
+        advanceDay(sulfurasProductService, item, 5, 80);
+        advanceDay(sulfurasProductService, item, 5, 80);
+        advanceDay(sulfurasProductService, item, 5, 80);
     }
 
     @Test
